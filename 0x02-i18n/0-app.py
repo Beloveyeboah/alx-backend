8f1c95f76debc860ab556a5e96fbe115d1696ef5
beloveyeboah <beloveyeboah@gmail.com>
2024-11-05 16:19:33 -0800

    zero +

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -11,7 +11,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Route for the index page"""
-    return render_template('index.html')
+    render_template('0-index.html')
 
 
-if __name__ == '__main__': app.run(debug=False)
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
